6e9fdcf92384c874310754a422e6a06e568cdc87
Leonardo Delcastanher <leodelcastanher@gmail.com>
2022-01-17 16:06:33 -0300

    Cleaned code of comments

--- src/app/shared/my-notification/my-notification.component.ts
+++ src/app/shared/my-notification/my-notification.component.ts
@@ -9,36 +9,6 @@ import {MyNotificationService} from "../../services/my-notification.service";
 })
 export class MyNotificationComponent implements OnInit {
   notifications: MyNotification[] = [];
-  // notifications: MyNotification[] = [
-  //   {
-  //     title: 'Notification Title',
-  //     message: 'Message here of your problem. Gotta fix this man...',
-  //     duration: 300,
-  //     type: 'error',
-  //     open: false
-  //   },
-  //   {
-  //     title: 'All Good Bro!',
-  //     message: 'Nice Work!',
-  //     duration: 300,
-  //     type: 'success',
-  //     open: false
-  //   },
-  //   {
-  //     title: 'Check Yoself',
-  //     message: 'Take a look in the mirror pal. Whats with the tie? Red belt? Zebra skin shoes...',
-  //     duration: 300,
-  //     type: 'warning',
-  //     open: false
-  //   },
-  //   {
-  //     title: 'It`s raining',
-  //     message: 'Really. Check it out!',
-  //     duration: 300,
-  //     type: 'info',
-  //     open: false
-  //   },
-  // ];
 
   constructor(public myNotification: MyNotificationService) {
   }
@@ -48,11 +18,6 @@ export class MyNotificationComponent implements OnInit {
       .subscribe((info) => {
         this.notifications = info;
       });
-    // this.notificationService.notification$
-    //   .pipe(takeUntil(this.destroyStream$))
-    //   .subscribe((message: string) => {
-    //     this.doSomeMagic(message)
-    //   })
   }
 
   getNotificationClasses(notification: MyNotification) {
